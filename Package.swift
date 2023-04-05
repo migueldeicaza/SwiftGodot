f3776cdfcd0d072d51a6377f2e64e0dee1d25bc6
Miguel de Icaza <miguel@gnome.org>
2023-04-04 22:33:29 -0400

    Try to streamline things by using a Swift Build Plugin: downside, it seems to rebuild everything on each build command

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
             name: "SwiftGodot",
             type: .dynamic,
             targets: ["SwiftGodot"]),
+        .plugin(name: "CodeGeneratorPlugin", targets: ["CodeGeneratorPlugin"]),
         .library(
             name: "SimpleExtension",
             type: .dynamic,
@@ -21,6 +22,12 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
+        .plugin(
+            name: "CodeGeneratorPlugin",
+            capability: .buildTool(),
+            dependencies: ["Generator"]
+        ),
+        .executableTarget(name: "Generator", path: "Generator"),
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
@@ -28,20 +35,20 @@ let package = Package(
             //exclude: ["include/README.md"]),
         .target(
             name: "SwiftGodot",
-            dependencies: ["GDExtension"],
+            dependencies: ["GDExtension", "Generator"],
             swiftSettings: [.unsafeFlags (["-suppress-warnings"])],
             linkerSettings: [.unsafeFlags (
                 ["-Xlinker", "-undefined",
                  "-Xlinker", "dynamic_lookup",
                  
-//                 "-Xlinker", "-mark_dead_strippable_dylib",
-//                 // This one is just to experiment for now
-//                 // Probably need to try the -why_live SYMBOL
-//                 "-Xlinker", "-dead_strip_dylibs",
-//                 "-Xlinker", "-why_live",
-//                 "-Xlinker", "_$s10SwiftGodot013AnimationNodeC0C8PlayModeOMf"
+                 //                 "-Xlinker", "-mark_dead_strippable_dylib",
+                 //                 // This one is just to experiment for now
+                 //                 // Probably need to try the -why_live SYMBOL
+                 //                 "-Xlinker", "-dead_strip_dylibs",
+                 //                 "-Xlinker", "-why_live",
+                 //                 "-Xlinker", "_$s10SwiftGodot013AnimationNodeC0C8PlayModeOMf"
                 ])
-            ]),
+            ], plugins: ["CodeGeneratorPlugin"]),
         .target(
             name: "SimpleExtension",
             dependencies: ["SwiftGodot"],
