1ff382cbcf29088e68d73e50ea94db332d45d3db
Miguel de Icaza <miguel@gnome.org>
2023-05-06 20:57:03 -0400

    Add missing files

---
+++ Sources/GDExtension/include/GDExtensionSupport.h
@@ -0,0 +1,16 @@
+//
+//  GDExtensionSupport.c.h
+//  
+//
+//  Created by Miguel de Icaza on 3/24/23.
+//
+
+#ifndef GDExtensionSupport_c_h
+#define GDExtensionSupport_c_h
+
+#include <stdio.h>
+#include <gdextension_interface.h>
+
+typedef GDExtensionBool (*DEMO_ENTRY_POINT)(const GDExtensionInterface *p_interface, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization);
+
+#endif /* GDExtensionSupport_c_h */
