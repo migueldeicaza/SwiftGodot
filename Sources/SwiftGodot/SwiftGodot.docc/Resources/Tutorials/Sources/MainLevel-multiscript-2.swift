ae1faa6f9abbb090449f88de16016ff8c5edd352
Marquis Kurt <software@marquiskurt.net>
2023-07-22 15:38:52 -0400

    Add main level tutorial
    This tutorial focuses on adding multiple classes, fetching nodes from
    the scene tree, and working with basic Godot signals.

---
+++ Sources/SwiftGodot/SwiftGodot.docc/Resources/Tutorials/Sources/MainLevel-multiscript-2.swift
@@ -0,0 +1,19 @@
+//
+//  MainLevel.swift
+//  
+//
+//  Created by Marquis Kurt on 7/22/23.
+//
+
+import Foundation
+import SwiftGodot
+
+class MainLevel: Node2D {
+    required init() {
+        super.init()
+    }
+
+    required init(nativeHandle: UnsafeRawPointer) {
+        fatalError("init(nativeHandle:) not implemented")
+    }
+}
