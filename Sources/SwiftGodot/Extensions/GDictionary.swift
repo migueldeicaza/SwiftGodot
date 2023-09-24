4f230abfd16ac0467d66c85c5e82982cdd2acd74
Miguel de Icaza <miguel@gnome.org>
2023-09-23 23:14:12 -0400

    GDictionary convenience

---
+++ Sources/SwiftGodot/Extensions/GDictionary.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  
+//
+//  Created by Miguel de Icaza on 9/23/23.
+//
+
+import Foundation
+
+public extension GDictionary {
+    /// Convenience subscript that uses a String as the key to access the
+    /// elements in the dictionary.   Merely wraps this on a Variant.
+    public subscript (key: String) -> Variant? {
+        get {
+            return self [Variant (key)]
+        }
+        set {
+            self [Variant (key)] = newValue
+        }
+    }
+    
+    /// Convenience subscript that uses a StringName as the key to access the
+    /// elements in the dictionary.   Merely wraps this on a Variant.
+    public subscript (key: StringName) -> Variant? {
+        get {
+            return self [Variant (key)]
+        }
+        set {
+            self [Variant (key)] = newValue
+        }
+    }
+}
