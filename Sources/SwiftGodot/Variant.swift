76aa293a0c146767300fea2a3c5842594256babe
Miguel de Icaza <miguel@gnome.org>
2023-09-25 23:49:13 -0400

    Add the double constructor for variants

--- Sources/SwiftGodot/Variant.swift
+++ Sources/SwiftGodot/Variant.swift
@@ -30,9 +30,9 @@
 /// - Can work as an exported property, so the editor can edit it universally.
 /// - Can be used for dictionaries, arrays, parsers, etc.
 ///
-/// > Note: Containers (``GArray`` and ``Dictionary``): Both are implemented using variants.
-/// A ``Dictionary`` can match any datatype used as key to any other datatype.  An ``GArray``
-/// just holds an array of Variants.  A ``Variant`` can also hold a ``Dictionary`` and an ``Array``
+/// > Note: Containers (``GArray`` and ``GDictionary``): Both are implemented using variants.
+/// A ``GDictionary`` can match any datatype used as key to any other datatype.  An ``GArray``
+/// just holds an array of Variants.  A ``Variant`` can also hold a ``GDictionary`` and an ``GArray``
 /// inside.
 ///
 /// Modifications to a container will modify all references to it.
@@ -133,7 +133,12 @@ public class Variant: Hashable, Equatable, ExpressibleByStringLiteral {
         var v = Double (value)
         Variant.fromTypeMap [GType.float.rawValue] (&content, &v)
     }
-    
+
+    public init (_ value: Double) {
+        var v = Double (value)
+        Variant.fromTypeMap [GType.float.rawValue] (&content, &v)
+    }
+
     public init (_ value: GString) {
         var v = GDExtensionStringPtr (&value.content)
         Variant.fromTypeMap [GType.string.rawValue] (&content, v)
