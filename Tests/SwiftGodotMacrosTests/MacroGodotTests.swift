daa4ea153898ca1f2c668aa4ccea0b2db5141903
Estevan Hernandez <esther65536@gmail.com>
2023-11-30 22:41:00 -0600

    fix whitespace / indentation

--- Tests/SwiftGodotMacrosTests/MacroGodotTests.swift
+++ Tests/SwiftGodotMacrosTests/MacroGodotTests.swift
@@ -191,72 +191,72 @@ final class MacroGodotTests: XCTestCase {
             }
             """,
             expandedSource:
-"""
-class Castro: Node {
-    func deleteEpisode() {}
+                """
+                class Castro: Node {
+                    func deleteEpisode() {}
 
-    func _mproxy_deleteEpisode (args: [Variant]) -> Variant? {
-    	deleteEpisode ()
-    	return nil
-    }
-    func subscribe(podcast: Podcast) {}
+                    func _mproxy_deleteEpisode (args: [Variant]) -> Variant? {
+                    	deleteEpisode ()
+                    	return nil
+                    }
+                    func subscribe(podcast: Podcast) {}
 
-    func _mproxy_subscribe (args: [Variant]) -> Variant? {
-    	subscribe (podcast: Podcast.makeOrUnwrap (args [0])!)
-    	return nil
-    }
-    func removeSilences(from: Variant) {}
+                    func _mproxy_subscribe (args: [Variant]) -> Variant? {
+                    	subscribe (podcast: Podcast.makeOrUnwrap (args [0])!)
+                    	return nil
+                    }
+                    func removeSilences(from: Variant) {}
 
-    func _mproxy_removeSilences (args: [Variant]) -> Variant? {
-    	removeSilences (from: args [0])
-    	return nil
-    }
-    func getLatestEpisode(podcast: Podcast) -> Episode {}
+                    func _mproxy_removeSilences (args: [Variant]) -> Variant? {
+                    	removeSilences (from: args [0])
+                    	return nil
+                    }
+                    func getLatestEpisode(podcast: Podcast) -> Episode {}
 
-    func _mproxy_getLatestEpisode (args: [Variant]) -> Variant? {
-    	let result = getLatestEpisode (podcast: Podcast.makeOrUnwrap (args [0])!)
-    	return Variant (result)
-    }
-    func queue(_ podcast: Podcast, after preceedingPodcast: Podcast) {}
+                    func _mproxy_getLatestEpisode (args: [Variant]) -> Variant? {
+                    	let result = getLatestEpisode (podcast: Podcast.makeOrUnwrap (args [0])!)
+                    	return Variant (result)
+                    }
+                    func queue(_ podcast: Podcast, after preceedingPodcast: Podcast) {}
 
-    func _mproxy_queue (args: [Variant]) -> Variant? {
-    	queue (Podcast.makeOrUnwrap (args [0])!, after: Podcast.makeOrUnwrap (args [1])!)
-    	return nil
-    }
+                    func _mproxy_queue (args: [Variant]) -> Variant? {
+                    	queue (Podcast.makeOrUnwrap (args [0])!, after: Podcast.makeOrUnwrap (args [1])!)
+                    	return nil
+                    }
 
-    override open class var classInitializer: Void {
-        let _ = super.classInitializer
-        return _initializeClass
-    }
+                    override open class var classInitializer: Void {
+                        let _ = super.classInitializer
+                        return _initializeClass
+                    }
 
-    private static var _initializeClass: Void = {
-        let className = StringName("Castro")
-        let classInfo = ClassInfo<Castro> (name: className)
-    	classInfo.registerMethod(name: StringName("deleteEpisode"), flags: .default, returnValue: nil, arguments: [], function: Castro._mproxy_deleteEpisode)
-    	let prop_0 = PropInfo (propertyType: .object, propertyName: "podcast", className: StringName("Podcast"), hint: .none, hintStr: "", usage: .default)
-    	let subscribeArgs = [
-    		prop_0,
-    	]
-    	classInfo.registerMethod(name: StringName("subscribe"), flags: .default, returnValue: nil, arguments: subscribeArgs, function: Castro._mproxy_subscribe)
-    	let prop_1 = PropInfo (propertyType: .object, propertyName: "from", className: StringName("Variant"), hint: .none, hintStr: "", usage: .default)
-    	let removeSilencesArgs = [
-    		prop_1,
-    	]
-    	classInfo.registerMethod(name: StringName("removeSilences"), flags: .default, returnValue: nil, arguments: removeSilencesArgs, function: Castro._mproxy_removeSilences)
-    	let prop_2 = PropInfo (propertyType: .object, propertyName: "", className: StringName("Episode"), hint: .none, hintStr: "", usage: .default)
-    	let getLatestEpisodeArgs = [
-    		prop_0,
-    	]
-    	classInfo.registerMethod(name: StringName("getLatestEpisode"), flags: .default, returnValue: prop_2, arguments: getLatestEpisodeArgs, function: Castro._mproxy_getLatestEpisode)
-    	let prop_3 = PropInfo (propertyType: .object, propertyName: "preceedingPodcast", className: StringName("Podcast"), hint: .none, hintStr: "", usage: .default)
-    	let queueArgs = [
-    		prop_0,
-    		prop_3,
-    	]
-    	classInfo.registerMethod(name: StringName("queue"), flags: .default, returnValue: nil, arguments: queueArgs, function: Castro._mproxy_queue)
-    } ()
-}
-""",
+                    private static var _initializeClass: Void = {
+                        let className = StringName("Castro")
+                        let classInfo = ClassInfo<Castro> (name: className)
+                    	classInfo.registerMethod(name: StringName("deleteEpisode"), flags: .default, returnValue: nil, arguments: [], function: Castro._mproxy_deleteEpisode)
+                    	let prop_0 = PropInfo (propertyType: .object, propertyName: "podcast", className: StringName("Podcast"), hint: .none, hintStr: "", usage: .default)
+                    	let subscribeArgs = [
+                    		prop_0,
+                    	]
+                    	classInfo.registerMethod(name: StringName("subscribe"), flags: .default, returnValue: nil, arguments: subscribeArgs, function: Castro._mproxy_subscribe)
+                    	let prop_1 = PropInfo (propertyType: .object, propertyName: "from", className: StringName("Variant"), hint: .none, hintStr: "", usage: .default)
+                    	let removeSilencesArgs = [
+                    		prop_1,
+                    	]
+                    	classInfo.registerMethod(name: StringName("removeSilences"), flags: .default, returnValue: nil, arguments: removeSilencesArgs, function: Castro._mproxy_removeSilences)
+                    	let prop_2 = PropInfo (propertyType: .object, propertyName: "", className: StringName("Episode"), hint: .none, hintStr: "", usage: .default)
+                    	let getLatestEpisodeArgs = [
+                    		prop_0,
+                    	]
+                    	classInfo.registerMethod(name: StringName("getLatestEpisode"), flags: .default, returnValue: prop_2, arguments: getLatestEpisodeArgs, function: Castro._mproxy_getLatestEpisode)
+                    	let prop_3 = PropInfo (propertyType: .object, propertyName: "preceedingPodcast", className: StringName("Podcast"), hint: .none, hintStr: "", usage: .default)
+                    	let queueArgs = [
+                    		prop_0,
+                    		prop_3,
+                    	]
+                    	classInfo.registerMethod(name: StringName("queue"), flags: .default, returnValue: nil, arguments: queueArgs, function: Castro._mproxy_queue)
+                    } ()
+                }
+                """,
 			macros: testMacros
 		)
 	}
@@ -271,62 +271,62 @@ class Castro: Node {
             }
             """,
             expandedSource:
-"""
-class MathHelper: Node {
-    func multiply(_ a: Int, by b: Int) -> Int { a * b}
+                """
+                class MathHelper: Node {
+                    func multiply(_ a: Int, by b: Int) -> Int { a * b}
 
-    func _mproxy_multiply (args: [Variant]) -> Variant? {
-    	let result = multiply (Int.makeOrUnwrap (args [0])!, by: Int.makeOrUnwrap (args [1])!)
-    	return Variant (result)
-    }
-    func divide(_ a: Float, by b: Float) -> Float { a / b }
+                    func _mproxy_multiply (args: [Variant]) -> Variant? {
+                    	let result = multiply (Int.makeOrUnwrap (args [0])!, by: Int.makeOrUnwrap (args [1])!)
+                    	return Variant (result)
+                    }
+                    func divide(_ a: Float, by b: Float) -> Float { a / b }
 
-    func _mproxy_divide (args: [Variant]) -> Variant? {
-    	let result = divide (Float.makeOrUnwrap (args [0])!, by: Float.makeOrUnwrap (args [1])!)
-    	return Variant (result)
-    }
-    func areBothTrue(_ a: Bool, and b: Bool) -> Bool { a == b }
+                    func _mproxy_divide (args: [Variant]) -> Variant? {
+                    	let result = divide (Float.makeOrUnwrap (args [0])!, by: Float.makeOrUnwrap (args [1])!)
+                    	return Variant (result)
+                    }
+                    func areBothTrue(_ a: Bool, and b: Bool) -> Bool { a == b }
 
-    func _mproxy_areBothTrue (args: [Variant]) -> Variant? {
-    	let result = areBothTrue (Bool.makeOrUnwrap (args [0])!, and: Bool.makeOrUnwrap (args [1])!)
-    	return Variant (result)
-    }
+                    func _mproxy_areBothTrue (args: [Variant]) -> Variant? {
+                    	let result = areBothTrue (Bool.makeOrUnwrap (args [0])!, and: Bool.makeOrUnwrap (args [1])!)
+                    	return Variant (result)
+                    }
 
-    override open class var classInitializer: Void {
-        let _ = super.classInitializer
-        return _initializeClass
-    }
+                    override open class var classInitializer: Void {
+                        let _ = super.classInitializer
+                        return _initializeClass
+                    }
 
-    private static var _initializeClass: Void = {
-        let className = StringName("MathHelper")
-        let classInfo = ClassInfo<MathHelper> (name: className)
-    	let prop_0 = PropInfo (propertyType: .int, propertyName: "", className: StringName(""), hint: .none, hintStr: "", usage: .default)
-    	let prop_1 = PropInfo (propertyType: .int, propertyName: "a", className: StringName(""), hint: .none, hintStr: "", usage: .default)
-    	let prop_2 = PropInfo (propertyType: .int, propertyName: "b", className: StringName(""), hint: .none, hintStr: "", usage: .default)
-    	let multiplyArgs = [
-    		prop_1,
-    		prop_2,
-    	]
-    	classInfo.registerMethod(name: StringName("multiply"), flags: .default, returnValue: prop_0, arguments: multiplyArgs, function: MathHelper._mproxy_multiply)
-    	let prop_3 = PropInfo (propertyType: .float, propertyName: "", className: StringName(""), hint: .none, hintStr: "", usage: .default)
-    	let prop_4 = PropInfo (propertyType: .float, propertyName: "a", className: StringName(""), hint: .none, hintStr: "", usage: .default)
-    	let prop_5 = PropInfo (propertyType: .float, propertyName: "b", className: StringName(""), hint: .none, hintStr: "", usage: .default)
-    	let divideArgs = [
-    		prop_4,
-    		prop_5,
-    	]
-    	classInfo.registerMethod(name: StringName("divide"), flags: .default, returnValue: prop_3, arguments: divideArgs, function: MathHelper._mproxy_divide)
-    	let prop_6 = PropInfo (propertyType: .bool, propertyName: "", className: StringName(""), hint: .none, hintStr: "", usage: .default)
-    	let prop_7 = PropInfo (propertyType: .bool, propertyName: "a", className: StringName(""), hint: .none, hintStr: "", usage: .default)
-    	let prop_8 = PropInfo (propertyType: .bool, propertyName: "b", className: StringName(""), hint: .none, hintStr: "", usage: .default)
-    	let areBothTrueArgs = [
-    		prop_7,
-    		prop_8,
-    	]
-    	classInfo.registerMethod(name: StringName("areBothTrue"), flags: .default, returnValue: prop_6, arguments: areBothTrueArgs, function: MathHelper._mproxy_areBothTrue)
-    } ()
-}
-""",
+                    private static var _initializeClass: Void = {
+                        let className = StringName("MathHelper")
+                        let classInfo = ClassInfo<MathHelper> (name: className)
+                    	let prop_0 = PropInfo (propertyType: .int, propertyName: "", className: StringName(""), hint: .none, hintStr: "", usage: .default)
+                    	let prop_1 = PropInfo (propertyType: .int, propertyName: "a", className: StringName(""), hint: .none, hintStr: "", usage: .default)
+                    	let prop_2 = PropInfo (propertyType: .int, propertyName: "b", className: StringName(""), hint: .none, hintStr: "", usage: .default)
+                    	let multiplyArgs = [
+                    		prop_1,
+                    		prop_2,
+                    	]
+                    	classInfo.registerMethod(name: StringName("multiply"), flags: .default, returnValue: prop_0, arguments: multiplyArgs, function: MathHelper._mproxy_multiply)
+                    	let prop_3 = PropInfo (propertyType: .float, propertyName: "", className: StringName(""), hint: .none, hintStr: "", usage: .default)
+                    	let prop_4 = PropInfo (propertyType: .float, propertyName: "a", className: StringName(""), hint: .none, hintStr: "", usage: .default)
+                    	let prop_5 = PropInfo (propertyType: .float, propertyName: "b", className: StringName(""), hint: .none, hintStr: "", usage: .default)
+                    	let divideArgs = [
+                    		prop_4,
+                    		prop_5,
+                    	]
+                    	classInfo.registerMethod(name: StringName("divide"), flags: .default, returnValue: prop_3, arguments: divideArgs, function: MathHelper._mproxy_divide)
+                    	let prop_6 = PropInfo (propertyType: .bool, propertyName: "", className: StringName(""), hint: .none, hintStr: "", usage: .default)
+                    	let prop_7 = PropInfo (propertyType: .bool, propertyName: "a", className: StringName(""), hint: .none, hintStr: "", usage: .default)
+                    	let prop_8 = PropInfo (propertyType: .bool, propertyName: "b", className: StringName(""), hint: .none, hintStr: "", usage: .default)
+                    	let areBothTrueArgs = [
+                    		prop_7,
+                    		prop_8,
+                    	]
+                    	classInfo.registerMethod(name: StringName("areBothTrue"), flags: .default, returnValue: prop_6, arguments: areBothTrueArgs, function: MathHelper._mproxy_areBothTrue)
+                    } ()
+                }
+                """,
 			macros: testMacros
 		)
     }
