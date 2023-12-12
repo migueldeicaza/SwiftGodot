ff78c577a1050ce64d8a728e9c51dff6452ec382
Estevan Hernandez <esther65536@gmail.com>
2023-12-11 19:58:09 -0600

    Revert "update macro and tests to include .array usage flag (#284)"
    This reverts commit 1d002efc8786a525c33a4d49db2ef99d73799016.

--- Sources/SwiftGodotMacroLibrary/MacroGodot.swift
+++ Sources/SwiftGodotMacroLibrary/MacroGodot.swift
@@ -299,7 +299,7 @@ class GodotMacroProcessor {
         className: StringName("\(godotArrayTypeName)"),
         hint: .\(f?.description ?? "none"),
         hintStr: \(s?.description ?? "\"\""),
-        usage: \(prefix == nil ? "[.default, .array]" : "[.default, .array, .group]"))\n
+        usage: \(prefix == nil ? ".default" : "[.default, .group]"))\n
     """)
             
             ctor.append("\tclassInfo.registerMethod (name: \"\(getterName)\", flags: .default, returnValue: \(pinfo), arguments: [], function: \(className).\(proxyGetterName))\n")
@@ -329,13 +329,13 @@ class GodotMacroProcessor {
                 previousPrefix = macroExpansion.exportGroupPrefix ?? ""
                 processExportGroup(name: name, prefix: previousPrefix ?? "")
             } else if let funcDecl = FunctionDeclSyntax(decl) {
-                try processFunction (funcDecl)
-            } else if let varDecl = VariableDeclSyntax(decl) {
-                if varDecl.isGArrayCollection {
+				try processFunction (funcDecl)
+			} else if let varDecl = VariableDeclSyntax(decl) {
+				if varDecl.isGArrayCollection {
                     try processGArrayCollectionVariable(varDecl, prefix: previousPrefix)
-                } else {
-                    try processVariable(varDecl, prefix: previousPrefix)
-                }
+				} else {
+					try processVariable(varDecl, prefix: previousPrefix)
+				}
             } else if let macroDecl = MacroExpansionDeclSyntax(decl) {
                 try classInitSignals(macroDecl)
             }
