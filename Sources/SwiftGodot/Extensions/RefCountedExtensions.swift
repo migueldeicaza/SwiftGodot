c2bc32fd227ceee794bd39fe4f284c8fa39ece75
Miguel de Icaza <miguel@gnome.org>
2024-02-08 12:29:30 -0500

    Add missing file

---
+++ Sources/SwiftGodot/Extensions/RefCountedExtensions.swift
@@ -0,0 +1,17 @@
+import Foundation
+@_implementationOnly import GDExtension
+
+public extension RefCounted {
+    /// Decrements the internal reference counter. Deletes underlying object if it goes to zero..
+    /// 
+    /// This is flagged `_exp` because it will eventually be folded into deinit for RefCounted
+    /// objects
+    public func _exp_unref() {
+        let instanceID = Int64(bitPattern: UInt64(getInstanceId()))
+        if GD.isInstanceIdValid(id: instanceID) {
+            if unreference() {
+                gi.object_destroy(UnsafeMutableRawPointer(mutating: handle))
+            }
+        }
+    }
+}
