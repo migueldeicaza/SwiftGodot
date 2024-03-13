3da971e5b2bb2ebd77c0bbd09fadfa0299b6a2e9
Miguel de Icaza <miguel@gnome.org>
2024-03-12 21:46:10 -0400

    GDictionary: Add debugging aid

--- Sources/SwiftGodot/Extensions/GDictionary.swift
+++ Sources/SwiftGodot/Extensions/GDictionary.swift
@@ -5,7 +5,7 @@
 //  Created by Miguel de Icaza on 9/23/23.
 //
 
-public extension GDictionary {
+public extension GDictionary: CustomDebugStringConvertible {
     /// Convenience subscript that uses a String as the key to access the
     /// elements in the dictionary.   Merely wraps this on a Variant.
     subscript (key: String) -> Variant? {
@@ -26,5 +26,10 @@ public extension GDictionary {
         set {
             self [Variant (key)] = newValue
         }
+    }
+
+    /// Renders the dictionary using the `Variant`'s `description` method.
+    public var debugDescription: String {
+        Variant (self).description
     }
 }
