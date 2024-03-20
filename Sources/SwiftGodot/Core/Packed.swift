89b84f3754136d0724b02209a951c05d2a6363ea
Miguel de Icaza <miguel@gnome.org>
2024-03-20 10:34:48 -0400

    PackedArrays convenience initializers, and a mutable version of the PackedByteArray unsafe mutator.
    This allows some Packed arrays to be constructed without creating an
    intermediate variant and an intermediate GArray to populate the arrays
    with values.
    
    More operations along these lines could be implemented (like slices
    directly into them, and adding support for more packed arrays, not
    just the handful of numeric types I did here).

--- Sources/SwiftGodot/Core/Packed.swift
+++ Sources/SwiftGodot/Core/Packed.swift
@@ -36,7 +36,7 @@ extension PackedByteArray {
             set (index: Int64 (index), value: Int64 (newValue))
         }
     }
-
+    
     /// Provides a mechanism to access the underlying data for the packed byte array
     ///
     /// - Parameter method: a callback that is invoked with a pointer to the underlying data, and
@@ -52,6 +52,38 @@ extension PackedByteArray {
             return method (ptr, Int (size ()))
         }
         return nil
+    }
+    
+    /// Provides a mechanism to access the underlying data for the packed byte array for mutation
+    ///
+    /// - Parameter method: a callback that is invoked with a pointer to the underlying data, and
+    /// the number of bytes in that block of data.   The callback is allowed to return nil it if fails to
+    /// do anything with the data.
+    ///
+    /// You could implement a method to access your data like this:
+    /// ```
+    /// let data: Data? = withUnsafeAccessToData { ptr, count in Data (ptr, count) }
+    /// ```
+    public func withUnsafeMutableAccessToData<T> (_ method: (_ pointer: UnsafeMutableRawPointer, _ count: Int)->T?) -> T? {
+        if let ptr = gi.packed_byte_array_operator_index(&content, 0) {
+            return method (ptr, Int (size ()))
+        }
+        return nil
+    }
+
+    /// Initializes a PackedByteArray from an array of UInt8 values.
+    public convenience init (_ data: [UInt8]) {
+        self.init ()
+        _ = resize(newSize: Int64(data.count))
+        if let ptr = gi.packed_byte_array_operator_index(&content, 0) {
+            ptr.withMemoryRebound(to: UInt8.self, capacity: data.count) { typed in
+                var idx = 0
+                for value in data {
+                    typed [idx] = value
+                    idx += 1
+                }
+            }
+        }
     }
 }
 
@@ -79,6 +111,21 @@ extension PackedFloat32Array {
             set (index: Int64 (index), value: Double (newValue))
         }
     }
+    
+    /// Initializes a PackedByteArray from an array of Float values.
+    public convenience init (_ data: [Float]) {
+        self.init ()
+        _ = resize(newSize: Int64(data.count))
+        if let ptr = gi.packed_float32_array_operator_index(&content, 0) {
+            ptr.withMemoryRebound(to: Float.self, capacity: data.count) { typed in
+                var idx = 0
+                for value in data {
+                    typed [idx] = value
+                    idx += 1
+                }
+            }
+        }
+    }
 }
 
 extension PackedFloat64Array {
@@ -92,6 +139,22 @@ extension PackedFloat64Array {
             set (index: Int64 (index), value: newValue)
         }
     }
+    
+    /// Initializes a PackedByteArray from an array of Double values.
+    public convenience init (_ data: [Double]) {
+        self.init ()
+        _ = resize(newSize: Int64(data.count))
+        if let ptr = gi.packed_float64_array_operator_index(&content, 0) {
+            ptr.withMemoryRebound(to: Double.self, capacity: data.count) { typed in
+                var idx = 0
+                for value in data {
+                    typed [idx] = value
+                    idx += 1
+                }
+            }
+        }
+    }
+
 }
 
 extension PackedInt32Array {
@@ -105,6 +168,21 @@ extension PackedInt32Array {
             set (index: Int64 (index), value: Int64(newValue))
         }
     }
+    
+    /// Initializes a PackedByteArray from an array of Int32 values values.
+    public convenience init (_ data: [Int32]) {
+        self.init ()
+        _ = resize(newSize: Int64(data.count))
+        if let ptr = gi.packed_int32_array_operator_index(&content, 0) {
+            ptr.withMemoryRebound(to: Int32.self, capacity: data.count) { typed in
+                var idx = 0
+                for value in data {
+                    typed [idx] = value
+                    idx += 1
+                }
+            }
+        }
+    }
 }
 
 extension PackedInt64Array {
@@ -116,6 +194,21 @@ extension PackedInt64Array {
         }
         set {
             set (index: Int64 (index), value: newValue)
+        }
+    }
+    
+    /// Initializes a PackedByteArray from an array of Int32 values values.
+    public convenience init (_ data: [Int64]) {
+        self.init ()
+        _ = resize(newSize: Int64(data.count))
+        if let ptr = gi.packed_int64_array_operator_index(&content, 0) {
+            ptr.withMemoryRebound(to: Int64.self, capacity: data.count) { typed in
+                var idx = 0
+                for value in data {
+                    typed [idx] = value
+                    idx += 1
+                }
+            }
         }
     }
 }
---
+++ Tests/SwiftGodotTests/BuiltIn/PackedArrayTests.swift
@@ -0,0 +1,47 @@
+//
+//  File.swift
+//  
+//
+//  Created by Miguel de Icaza on 3/20/24.
+//
+
+import Foundation
+import XCTest
+import SwiftGodotTestability
+@testable import SwiftGodot
+
+final class PackedArrayTests: GodotTestCase {
+    func testCustomInitializes () {
+        let bytes: [UInt8] = [10, 20, 30, 255, 0, 3]
+        
+        let a = PackedByteArray (bytes)
+        for (idx, v) in bytes.enumerated () {
+            XCTAssertEqual (v, a [idx])
+        }
+        
+        let ints: [Int32] = [10, 1024, 0xf00dca7, 0, 23, Int32.max, Int32.min]
+        let b = PackedInt32Array (ints)
+        for (idx, v) in ints.enumerated () {
+            XCTAssertEqual (v, b [idx])
+        }
+
+        let longs: [Int64] = [10, 1024, 0xf00dca7dead, 0, Int64.max, Int64.min]
+        let c = PackedInt64Array (longs)
+        for (idx, v) in longs.enumerated () {
+            XCTAssertEqual (v, c [idx])
+        }
+
+        let floats: [Float] = [0, 0.3, .pi, 0.3, 1000000.3]
+        let d = PackedFloat32Array(floats)
+        for (idx, v) in floats.enumerated() {
+            XCTAssertEqual (v, d [idx])
+        }
+
+        let doubles: [Double] = [0, 0.3, .pi, 0.3, 1000000.3]
+        let e = PackedFloat64Array(doubles)
+        for (idx, v) in doubles.enumerated() {
+            XCTAssertEqual (v, e [idx])
+        }
+
+    }
+}
