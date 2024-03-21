1b64f3e580940ab669c1cd08d13d43f307dcc386
Dandy Griffin <dan@contagious.nu>
2024-03-21 13:41:16 -0500

    Updates BindNode with safety and convenience features
    Including:
    - use nodeName to manually define the named Scene node.
    - allow BindNode to look up the Godot eitor case naming style (Pascal case) despite camelCased Swift vars
    - Makes BindNode require an Optional to prevent cases where mismatched name lookups or access in ready cause a crash

--- Sources/SwiftGodot/Extensions/NodeExtensions.swift
+++ Sources/SwiftGodot/Extensions/NodeExtensions.swift
@@ -19,25 +19,53 @@
 ///
 ///     getNode(path: NodeName("GameOverLabel")) as! Label
 
+/// Use the BindNode property wrapper in any subclass of Node to retrieve the node from the
+/// current container that matches the name of the property.
+///
+/// For example:
+///
+///     class MyElements: CanvasLayer {
+///         @BindNode var GameOverLabel: Label
+///         @BindNode var gameOverLabel: Label
+///
+///         @BindNode(nodeName: "GameOverLabel") var customLabel: Label
+///     }
+///
+/// The above is equivalent to calling
+///
+///     getNode(path: NodeName("GameOverLabel")) as! Label
+
 @propertyWrapper
 public struct BindNode<Value: Node> {
     public static subscript<T: Node>(
           _enclosingInstance instance: T,
-          wrapped wrappedKeyPath: ReferenceWritableKeyPath<T, Value>,
+          wrapped wrappedKeyPath: ReferenceWritableKeyPath<T, Value?>,
           storage storageKeyPath: ReferenceWritableKeyPath<T, Self>
-    ) -> Value {
+    ) -> Value? {
         get {
             if #available(macOS 13.3, iOS 16.4, tvOS 16.4, *){
-                let name: String
-                let fullName = wrappedKeyPath.debugDescription
-                if let namePos = fullName.lastIndex(of: ".") {
-                    name = String (fullName [fullName.index(namePos, offsetBy: 1)...])
-                } else {
-                    name = fullName
-                }
-                let nodePath = NodePath(from: name)
-                
-                return instance.getNode(path: nodePath) as! Value
+                let discoveredName: String = {
+                    if let nodeName = instance[keyPath: storageKeyPath].nodeName {
+                        return nodeName
+                    }
+
+                    // Check original variable name
+                    let cleanedDebugName = makeCleanName(from: wrappedKeyPath.debugDescription)
+                    guard !instance.hasNode(path: NodePath(from: cleanedDebugName)) else {
+                        return cleanedDebugName
+                    }
+
+                    // Check lower/upper cased first character variants to bridge the casing gap with
+                    // swift variables and GD scene names.
+                    let firstCharacterCaseVariantNames = makeVariantNames(from: cleanedDebugName)
+                    if instance.hasNode(path: NodePath(from: firstCharacterCaseVariantNames.camel)) {
+                        return firstCharacterCaseVariantNames.camel
+                    } else {
+                        return firstCharacterCaseVariantNames.pascal
+                    }
+                }()
+
+                return instance.getNode(path: NodePath(from: discoveredName)) as? Value
             } else {
                 fatalError ("BindNode is not supported with current swift, or older Mac")
             }
@@ -46,16 +74,29 @@ public struct BindNode<Value: Node> {
             fatalError()
         }
     }
-    
-    public init () {}
-    @available(*, unavailable, message: "This property wrapper can only be applied to classes")
-    public var wrappedValue: Value {
-        get {
-            fatalError()
-        }
-        set {
-            fatalError()
+
+    private static func makeCleanName(from debugDescriptionName: String) -> String {
+        if let namePos = debugDescriptionName.lastIndex(of: ".") {
+            return String (debugDescriptionName [debugDescriptionName.index(namePos, offsetBy: 1)...])
+        } else {
+            return debugDescriptionName
         }
     }
-}
 
+    private static func makeVariantNames(from name: String) -> (camel: String, pascal: String) {
+        let camel = name.prefix(1).lowercased() + name.dropFirst()
+        let pascal = name.prefix(1).uppercased() + name.dropFirst()
+        return (camel, pascal)
+    }
+
+    var nodeName: String?
+    public init(nodeName: String? = nil) {
+        self.nodeName = nodeName
+    }
+
+    @available(*, unavailable, message: "This property wrapper can only be applied to classes")
+    public var wrappedValue: Value? {
+        get { fatalError() }
+        set { fatalError() }
+    }
+}
