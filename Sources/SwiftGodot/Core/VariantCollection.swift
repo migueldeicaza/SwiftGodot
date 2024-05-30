164dcde76d8ad65a222e794f91fdfe6bded6db10
Joey <jnelson@evluma.com>
2024-05-30 11:04:53 -0400

    updated internal usage

--- Sources/SwiftGodot/Core/VariantCollection.swift
+++ Sources/SwiftGodot/Core/VariantCollection.swift
@@ -25,7 +25,7 @@ public class VariantCollection<Element: VariantStorable>: Collection, Expressibl
     /// Initializes the collection using an array literal, for example: `let variantCollection: VariantCollection<Int> = [0]`
     public required init(arrayLiteral elements: ArrayLiteralElement...) {
 		array = elements.reduce(into: .init(Element.self)) {
-			$0.append(value: Variant($1))
+            $0.append(Variant($1))
 		}
     }
     
@@ -126,7 +126,7 @@ public class VariantCollection<Element: VariantStorable>: Collection, Expressibl
     
     /// Appends an element at the end of the array (alias of ``pushBack(value:)``).
     public final func append (value: Element) {
-        array.append (value: Variant(value))
+        array.append (Variant(value))
     }
     
     /// Resizes the array to contain a different number of elements. If the array size is smaller, elements are cleared, if bigger, new elements are `null`. Returns ``GodotError/ok`` on success, or one of the other ``GodotError`` values if the operation failed.
