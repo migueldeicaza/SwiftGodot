3013c2984396539d3f53eafe9c12e5b9593df7f7
Estevan Hernandez <esther65536@gmail.com>
2024-06-24 21:48:06 -0500

    feat: adds InersectRayResult helper to PhysicsDirectSpaceState2D

---
+++ Tests/SwiftGodotTests/PhysicsDirectSpaceState2DIntersectRayResultTests.swift
@@ -0,0 +1,56 @@
+//
+//  PhysicsDirectSpaceState2DIntersectRayResultTests.swift
+//  SwiftGodotTests
+//
+//  Created by Estevan Hernandez on 12/24/23.
+//
+
+import XCTest
+import SwiftGodotTestability
+@testable import SwiftGodot
+
+final class PhysicsDirectSpaceState2DIntersectRayResultTests: GodotTestCase {
+    func testIntersectRayResultPropertiesMatchDictionary_whenAllPropertiesPresent() throws {
+        let collider: Object = GridMap()
+        
+        let dictionary: GDictionary = {
+            let dictionary = GDictionary()
+            dictionary["position"] = Variant(Vector2(x: 1, y: 2, z: 3))
+            dictionary["normal"] = Variant(Vector2(x: 4, y: 5, z: 6))
+            dictionary["collider"] = Variant(collider)
+            dictionary["collider_id"] = Variant(collider.id)
+            dictionary["rid"] = Variant(RID())
+            dictionary["shape"] = Variant(22)
+            return dictionary
+        }()
+        
+        let result = try XCTUnwrap(PhysicsDirectSpaceState2D.IntersectRayResult<GridMap>(dictionary))
+        
+        XCTAssertEqual(result.position, Vector2(x: 1, y: 2, z: 3))
+        XCTAssertEqual(result.normal, Vector2(x: 4, y: 5, z: 6))
+        XCTAssertEqual(result.collider, collider)
+        XCTAssertEqual(result.colliderId, collider.id)
+        XCTAssertEqual(result.rid, RID())
+        XCTAssertEqual(result.shape, 22)
+        XCTAssertEqual(result.faceIndex, 44)
+    }
+    
+    func testIntersectRayResultIsNil_whenColliderPropertyIsMissing() {
+        let collider: Object = GridMap()
+        
+        let dictionary: GDictionary = {
+            let dictionary = GDictionary()
+            dictionary["position"] = Variant(Vector2(x: 1, y: 2, z: 3))
+            dictionary["normal"] = Variant(Vector2(x: 4, y: 5, z: 6))
+//            dictionary["collider"] = Variant(collider)
+            dictionary["collider_id"] = Variant(collider.id)
+            dictionary["rid"] = Variant(RID())
+            dictionary["shape"] = Variant(22)
+            return dictionary
+        }()
+        
+        let result = PhysicsDirectSpaceState2D.IntersectRayResult<GridMap>(dictionary)
+        
+        XCTAssertNil(result)
+    }
+}
