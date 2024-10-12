3899f28fe3fedb23dca253b959884ff8ef492485
Elijah Semyonov <elijah.semenov@gmail.com>
2024-10-12 14:02:57 +0900

    Update MacroCallableIntegrationTests.swift

--- Tests/SwiftGodotTests/MacroCallableIntegrationTests.swift
+++ Tests/SwiftGodotTests/MacroCallableIntegrationTests.swift
@@ -15,11 +15,15 @@ fileprivate class TestObject: Object {
     func countObjects(_ objects: ObjectCollection<TestObject>) -> Int {
         return objects.count
     }
+    
+    @Callable
+    func countBuiltins(_ builtins: VariantCollection<Int>) -> Int {
+        return builtins.count
+    }
 }
 
 @Godot
-fileprivate class TestObject2: TestObject {
-    
+fileprivate class TestObject2: TestObject { // for checking inheritance
 }
 
 final class MacroCallableIntegrationTests: GodotTestCase {
@@ -62,11 +66,11 @@ final class MacroCallableIntegrationTests: GodotTestCase {
         objectsArray.append(nil)
         objectsArray.append(Variant(RefCounted())) // this one causes the failure
         
-        // Fails, prints into consoke and returns nil due to RefCounted not being TestObject
+        // Fails, prints into console and returns nil due to RefCounted not being TestObject
         XCTAssertEqual(testObject.call(method: "countObjects", Variant(objectsArray)), nil)
     }
     
-    func testExplicitTypingOfUntypedObjectArrayFailure() {
+    func testExplicitlyTypedObjectArrayGodotSideMismatch() {
         let testObject = TestObject()
         
         let object0 = TestObject()
@@ -80,9 +84,9 @@ final class MacroCallableIntegrationTests: GodotTestCase {
         objectsArray.append(nil) // 4
         objectsArray.append(Variant(object2)) // 5
         objectsArray.append(nil) // 6
-        objectsArray.append(Variant(RefCounted())) // this one won't be added with a error log from Godot
         
-        // Fails, prints into consoke and returns nil due to RefCounted not being TestObject
+        // this one won't be added, error is logged from Godot, RefCounted is not TestObject
+        objectsArray.append(Variant(RefCounted()))
         XCTAssertEqual(testObject.call(method: "countObjects", Variant(objectsArray)), Variant(6))
     }
     
@@ -102,5 +106,57 @@ final class MacroCallableIntegrationTests: GodotTestCase {
         objectsArray.append(nil) // 6
         
         XCTAssertEqual(testObject.call(method: "countObjects", Variant(objectsArray)), Variant(6))
+    }
+    
+    func testImplicitlyTypingBuiltinsArray() {
+        let testObject = TestObject()
+        
+        let builtinsArray = GArray()
+        builtinsArray.append(Variant(1))
+        builtinsArray.append(Variant(2))
+        builtinsArray.append(Variant(3))
+        
+        XCTAssertEqual(testObject.call(method: "countBuiltins", Variant(builtinsArray)), Variant(3))
+    }
+    
+    
+    func testImplicitTypingOfUntypedBuiltinArrayFailure() {
+        let testObject = TestObject()
+        
+        let array = GArray()
+        array.append(nil)
+        array.append(Variant(1))
+        array.append(Variant(2))
+        array.append(nil)
+        array.append(Variant(3))
+        array.append(nil)
+        array.append(Variant(4)) // this one causes the failure
+        
+        // Fails, prints into console and returns nil due to typed builtin array not allowing nils
+        XCTAssertEqual(testObject.call(method: "countObjects", Variant(array)), nil)
+    }
+    
+    func testExplicitlyTypedBuiltinArrayGodotSideMismatch() {
+        let testObject = TestObject()
+        
+        let builtinsArray = GArray(Int.self)
+        builtinsArray.append(Variant(1))
+        builtinsArray.append(Variant(2))
+        builtinsArray.append(Variant(3))
+        // this one won't be added, error is logged from Godot, nil Builtins are not allowed in typed Arrays
+        builtinsArray.append(nil)
+        
+        XCTAssertEqual(testObject.call(method: "countBuiltins", Variant(builtinsArray)), Variant(3))
+    }
+    
+    func testExplicitlyTypedBuiltinArray() {
+        let testObject = TestObject()
+        
+        let builtinsArray = VariantCollection<Int>()
+        builtinsArray.append(1)
+        builtinsArray.append(2)
+        builtinsArray.append(3)
+        
+        XCTAssertEqual(testObject.call(method: "countBuiltins", Variant(builtinsArray)), Variant(3))
     }
 }
