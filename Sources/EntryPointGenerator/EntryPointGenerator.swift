9834238148242b49cd77e597262f1d2b775c45c9
Elijah Semyonov <elijah.semenov@gmail.com>
2024-10-17 16:58:43 +0200

    Update EntryPointGenerator.swift

--- Sources/EntryPointGenerator/EntryPointGenerator.swift
+++ Sources/EntryPointGenerator/EntryPointGenerator.swift
@@ -15,13 +15,13 @@ class GodotMacroSearchingVisitor: SyntaxVisitor {
     
     override func visit(_ classDecl: ClassDeclSyntax) -> SyntaxVisitorContinueKind {
         // Check for attached macros (attributes)
-        let attributes = classDecl.attributes
-        
         for attribute in classDecl.attributes {
             if let attributeSyntax = attribute.as(AttributeSyntax.self) {
                 let attributeName = attributeSyntax.attributeName.description.trimmingCharacters(in: .whitespacesAndNewlines)
                 if attributeName == "Godot" {
-                    classes.append(classDecl.name.text)
+                    let className = classDecl.name.text
+                    print("Found \(className) with @Godot macro.")
+                    classes.append(className)
                     break // Found the @Godot macro, no need to check further
                 }
             }
@@ -44,6 +44,7 @@ struct EntryPointGenerator: ParsableCommand {
         let visitor = GodotMacroSearchingVisitor(viewMode: .all)
         
         for file in sourceFiles {
+            print("Scanning \(file)...")
             let source = try String(contentsOf: URL(fileURLWithPath: file))
             let fileSyntax = Parser.parse(source: source)
             
@@ -65,7 +66,9 @@ struct EntryPointGenerator: ParsableCommand {
         
         """
         
+        print("Writing \(outputFile)...")
         let outputURL = URL(fileURLWithPath: outputFile)
         try source.write(to: outputURL, atomically: true, encoding: .utf8)
+        print("Success! Entry point is `swift_entry_point`.")
     }
 }
