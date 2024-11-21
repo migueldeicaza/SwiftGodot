c09587c4401c9e40b948d17742b85204beff595f
Rob Mayoff <mayoff@dqd.com>
2024-11-21 15:41:46 -0600

    wip: write Vector2i tests using checkCover

--- Tests/SwiftGodotTests/BuiltIn/Vector2iTests.swift
+++ Tests/SwiftGodotTests/BuiltIn/Vector2iTests.swift
@@ -12,74 +12,78 @@ import RegexBuilder
 
 final class Vector2iTests: GodotTestCase {
 
-    static let rx_ws = ZeroOrMore { ChoiceOf { " "; "\t" } }
-    static let prefixCapture = Reference<Substring>()
-    static let suffixCapture = Reference<Substring>()
-    static let rx_check: Regex = Regex {
-        Anchor.startOfLine
-        Capture(as: prefixCapture) {
-            rx_ws
-            "check("
-            OneOrMore {
-                CharacterClass.anyNonNewline
+    static let testInt32s: [Int32] = [
+        .min,
+        -2,
+        -1,
+        0,
+        1,
+        2,
+        .max,
+    ]
+
+    static let testVectors: [Vector2i] = testInt32s.flatMap { y in
+        testInt32s.map { x in
+            Vector2i(x: x, y: y)
+        }
+    }
+
+    func testInitFromVector2i() throws {
+        for y in Self.testInt32s {
+            try checkCover { Vector2i(from: Vector2i(x: 0, y: y)) }
+        }
+    }
+
+    func testInitFromVector2() throws {
+        for y in Self.testInt32s {
+            try checkCover { Vector2i(from: Vector2(x: 0, y: Float(y))) }
+        }
+
+        for y: Float in [-.infinity, -1e25, -0.0, 1e25, .infinity, .nan] {
+            try checkCover { Vector2i(from: Vector2(x: 0, y: y)) }
+        }
+    }
+
+    func testAspect() throws {
+        for v in Self.testVectors {
+            try checkCover { v.aspect() }
+        }
+    }
+
+    func testMaxAxisIndex() throws {
+        for v in Self.testVectors {
+            try checkCover { v.maxAxisIndex() }
+        }
+    }
+
+    func testMinAxisIndex() throws {
+        for v in Self.testVectors {
+            try checkCover { v.minAxisIndex() }
+        }
+    }
+
+    func testDistanceTo() throws {
+        for v in Self.testVectors {
+            for u in Self.testVectors {
+                try checkCover { v.distanceTo(u) }
             }
-            ", is:"
-            rx_ws
         }
-        "\""
-        ZeroOrMore {
-            CharacterClass.anyNonNewline
-        }
-        "\""
-        Capture(as: suffixCapture) {
-            ")"
-            rx_ws
-        }
-        Anchor.endOfLine
     }
 
-#if CUSTOM_BUILTIN_IMPLEMENTATIONS
-    // I only update the test cases if I'm calling the Godot engine, which is authoritative. In this case, I'm using Swift covers, and the point of the tests is to verify that the covers match the engine's behavior. So it would be incorrect to update the test cases in this case.
-    static let shouldUpdateTestCases: Bool = false
-#else
-    static let shouldUpdateTestCases: Bool = ProcessInfo.processInfo.environment["UpdateSwiftCoverTestCases", default: ""] != ""
-#endif
-
-    func check<V>(
-        _ actual: V,
-        is expected: String,
-        file: StaticString = #filePath,
-        lineNumber: UInt = #line
-    ) {
-        guard Self.shouldUpdateTestCases else {
-            XCTAssertEqual(String(describing: actual), expected, file: file, line: lineNumber)
-            return
+    func testDistanceSquaredTo() throws {
+        for v in Self.testVectors {
+            for u in Self.testVectors {
+                try checkCover { v.distanceSquaredTo(u) }
+            }
         }
-
-        // I'm using Godot engine functions, not Swift cover implementations.
-        // I treat the Godot engine as a test oracle: it produces the correct answers.
-
-        let originalFile = try! String(contentsOfFile: file.description)
-        var lines = originalFile.split(separator: "\n", omittingEmptySubsequences: false)
-        let line = String(lines[Int(lineNumber) - 1])
-        guard let match = line.wholeMatch(of: Self.rx_check) else {
-            XCTFail("couldn't match check call", file: file, line: lineNumber)
-            return
-        }
-        // String(reflecting:) quotes and escapes the description of `actual`.
-        let newExpected = String(reflecting: String(describing: actual))
-        let newLine = "\(match[Self.prefixCapture])\(newExpected)\(match[Self.suffixCapture])"
-        lines[Int(lineNumber) - 1] = newLine[...]
-        let newFile = lines.joined(separator: "\n")
-        try! newFile.write(toFile: file.description, atomically: true, encoding: .utf8)
     }
 
-    func testInitFrom() {
-        check(Vector2i(from: Vector2i(x: 0, y: 0)), is: "Vector2i(x: 0, y: 0)")
-        check(Vector2i(from: Vector2i(x: 1, y: 1)), is: "Vector2i(x: 1, y: 1)")
-        check(Vector2i(from: Vector2i(x: 2, y: 2147483647)), is: "Vector2i(x: 2, y: 2147483647)")
-        check(Vector2i(from: Vector2i(x: 3, y: -1)), is: "Vector2i(x: 3, y: -1)")
-        check(Vector2i(from: Vector2i(x: 4, y: -2147483648)), is: "Vector2i(x: 4, y: -2147483648)")
+    func testPlus() throws {
+        for v in Self.testVectors {
+            for u in Self.testVectors {
+                try checkCover { v + u }
+            }
+        }
     }
 
     func testOperatorUnaryMinus () {
