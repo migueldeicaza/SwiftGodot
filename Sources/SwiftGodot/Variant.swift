7f0e392fd297727007df73ec26ae7c87ce79f041
Sam Deane <sam@elegantchaos.com>
2024-12-05 12:53:56 +0000

    tweaked unpack

--- Sources/SwiftGodot/Variant.swift
+++ Sources/SwiftGodot/Variant.swift
@@ -169,6 +169,12 @@ public class Variant: Hashable, Equatable, CustomDebugStringConvertible {
         return ret
     }
     
+    /// Fallback for when the type is not an object.
+    /// Will always return nil.
+    public func asObject<T: Any> (_ type: T.Type) -> T? {
+        return nil
+    }
+
     public var description: String {
         var ret = GDExtensionStringPtr (bitPattern: 0xdeaddead)
         gi.variant_stringify (&content, &ret)
