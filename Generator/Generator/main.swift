fc3c72b4f5b35297e380e82e3f81782641163ce4
Sam Deane <sam@elegantchaos.com>
2024-12-18 16:27:02 +0000

    split using ranges

--- Generator/Generator/main.swift
+++ Generator/Generator/main.swift
@@ -1,3 +1,4 @@
+import ExtensionApi
 //
 //  main.swift
 //  SwiftGodot/Generator
@@ -6,48 +7,48 @@
 //  Copyright © 2020-2023 Miguel de Icaza. MIT Licensed
 //
 import Foundation
-import ExtensionApi
 
 var args = CommandLine.arguments
 
 var rootUrl: URL {
-    let url = URL(fileURLWithPath: #file) // SwiftGodot/Generator/Generator/main.swift
-        .deletingLastPathComponent() // SwiftGodot/Generator/Generator
-        .deletingLastPathComponent() // SwiftGodot/Generator
-        .deletingLastPathComponent() // SwiftGodot
-    return url
+  let url = URL(fileURLWithPath: #file)  // SwiftGodot/Generator/Generator/main.swift
+    .deletingLastPathComponent()  // SwiftGodot/Generator/Generator
+    .deletingLastPathComponent()  // SwiftGodot/Generator
+    .deletingLastPathComponent()  // SwiftGodot
+  return url
 }
 
 var defaultExtensionApiJsonUrl: URL {
-    rootUrl
-        .appendingPathComponent("Sources")
-        .appendingPathComponent("ExtensionApi")
-        .appendingPathComponent("extension_api.json")
+  rootUrl
+    .appendingPathComponent("Sources")
+    .appendingPathComponent("ExtensionApi")
+    .appendingPathComponent("extension_api.json")
 }
 
 var defaultGeneratorOutputlUrl: URL {
-    rootUrl
-        .appendingPathComponent("GeneratedForDebug")
-        .appendingPathComponent("Sources")
+  rootUrl
+    .appendingPathComponent("GeneratedForDebug")
+    .appendingPathComponent("Sources")
 }
 
 var defaultDocRootUrl: URL {
-    rootUrl
-        .appendingPathComponent("GeneratedForDebug")
-        .appendingPathComponent("Docs")
+  rootUrl
+    .appendingPathComponent("GeneratedForDebug")
+    .appendingPathComponent("Docs")
 }
 
-let jsonFile = args.count > 1 ? args [1] : defaultExtensionApiJsonUrl.path
-var generatorOutput = args.count > 2 ? args [2] : defaultGeneratorOutputlUrl.path
-var docRoot =  args.count > 3 ? args [3] : defaultDocRootUrl.path
-let outputDir = args.count > 2 ? args [2] : generatorOutput
-let generateResettableCache = false 
+let jsonFile = args.count > 1 ? args[1] : defaultExtensionApiJsonUrl.path
+var generatorOutput = args.count > 2 ? args[2] : defaultGeneratorOutputlUrl.path
+var docRoot = args.count > 3 ? args[3] : defaultDocRootUrl.path
+let outputDir = args.count > 2 ? args[2] : generatorOutput
+let generateResettableCache = false
 
 // IF we want a single file, or one file per type
 var singleFile = args.contains("--singlefile")
 
 if args.count < 2 {
-    print("""
+  print(
+    """
     Usage is: generator path-to-extension-api output-directory doc-directory
     - path-to-extension-api is the full path to extension_api.json from Godot
     - output-directory is where the files will be placed
@@ -62,54 +63,54 @@ if args.count < 2 {
 let jsonData = try! Data(url: URL(fileURLWithPath: jsonFile))
 let jsonApi = try! JSONDecoder().decode(JGodotExtensionAPI.self, from: jsonData)
 
-func dropMatchingPrefix (_ enumName: String, _ enumKey: String) -> String {
-    let snake = snakeToCamel (enumKey)
-    if snake.lowercased().starts(with: enumName.lowercased()) {
-        if snake.count == enumName.count {
-            return snake
-        }
-        let ret = String (snake [snake.index (snake.startIndex, offsetBy: enumName.count)...])
-        if let f = ret.first {
-            if f.isNumber {
-                return snake
-            }
-        }
-        if ret == "" {
-            return snake
-        }
-        return ret.first!.lowercased() + ret.dropFirst()
+func dropMatchingPrefix(_ enumName: String, _ enumKey: String) -> String {
+  let snake = snakeToCamel(enumKey)
+  if snake.lowercased().starts(with: enumName.lowercased()) {
+    if snake.count == enumName.count {
+      return snake
     }
-    return snake
+    let ret = String(snake[snake.index(snake.startIndex, offsetBy: enumName.count)...])
+    if let f = ret.first {
+      if f.isNumber {
+        return snake
+      }
+    }
+    if ret == "" {
+      return snake
+    }
+    return ret.first!.lowercased() + ret.dropFirst()
+  }
+  return snake
 }
 
 var globalEnums: [String: JGodotGlobalEnumElement] = [:]
 
 // Maps from a the class name to its definition
-var classMap: [String:JGodotExtensionAPIClass] = [:]
+var classMap: [String: JGodotExtensionAPIClass] = [:]
 
 // Tracks whether a Godot type has subclasses, we want to use this
 // to determine whether we want to perform the more expensive lookup
 // for handle -> Swift type using `lookupObject` rather than creating
 // a plain wrapper directly from the handle
-var hasSubclasses = Set<String> ()
+var hasSubclasses = Set<String>()
 
 for x in jsonApi.classes {
-    classMap [x.name] = x
-    if let parentClass = x.inherits {
-        hasSubclasses.insert(parentClass)
-    }
+  classMap[x.name] = x
+  if let parentClass = x.inherits {
+    hasSubclasses.insert(parentClass)
+  }
 }
 
-fileprivate var structTypes: Set<String> = [
-    "const void*",
-    "AudioFrame*",
-    "Float",
-    "Int",
-    "float",
-    "int",
-    "Int32",
-    "Bool",
-    "bool",
+private var structTypes: Set<String> = [
+  "const void*",
+  "AudioFrame*",
+  "Float",
+  "Int",
+  "float",
+  "int",
+  "Int32",
+  "Bool",
+  "bool",
 ]
 
 /// - parameter type: A type name as found in `extension_api.json`.
@@ -119,38 +120,38 @@ func isStruct(_ type: String) -> Bool { structTypes.contains(type) }
 var builtinMap: [String: JGodotBuiltinClass] = [:]
 
 for x in jsonApi.builtinClasses {
-    if x.members?.count ?? 0 > 0 {
-        structTypes.insert(x.name)
-    }
-    builtinMap[x.name] = x
+  if x.members?.count ?? 0 > 0 {
+    structTypes.insert(x.name)
+  }
+  builtinMap[x.name] = x
 }
 
 let buildConfiguration: String = "float_64"
 var builtinSizes: [String: Int] = [:]
 for cs in jsonApi.builtinClassSizes {
-    if cs.buildConfiguration == buildConfiguration {
-        for c in cs.sizes {
-            builtinSizes [c.name] = c.size
-        }
+  if cs.buildConfiguration == buildConfiguration {
+    for c in cs.sizes {
+      builtinSizes[c.name] = c.size
     }
+  }
 }
 var builtinMemberOffsets: [String: [JGodotMember]] = [:]
 for mo in jsonApi.builtinClassMemberOffsets {
-    if mo.buildConfiguration == buildConfiguration {
-        for c in mo.classes {
-            builtinMemberOffsets [c.name.rawValue] = c.members
-        }
+  if mo.buildConfiguration == buildConfiguration {
+    for c in mo.classes {
+      builtinMemberOffsets[c.name.rawValue] = c.members
     }
+  }
 }
 
 let generatedBuiltinDir: String? = singleFile ? nil : (outputDir + "/generated-builtin/")
 let generatedDir: String? = singleFile ? nil : (outputDir + "/generated/")
 
 if singleFile {
-    try! FileManager.default.createDirectory(atPath: outputDir, withIntermediateDirectories: true)
+  try! FileManager.default.createDirectory(atPath: outputDir + "/generated/", withIntermediateDirectories: true)
 } else if let generatedBuiltinDir, let generatedDir {
-    try! FileManager.default.createDirectory(atPath: generatedBuiltinDir, withIntermediateDirectories: true)
-    try! FileManager.default.createDirectory(atPath: generatedDir, withIntermediateDirectories: true)
+  try! FileManager.default.createDirectory(atPath: generatedBuiltinDir, withIntermediateDirectories: true)
+  try! FileManager.default.createDirectory(atPath: generatedDir, withIntermediateDirectories: true)
 }
 
 //#if os(Windows)
@@ -163,33 +164,34 @@ if singleFile {
 //#endif
 
 struct Generator {
-    func run() async throws {
-        let coreDefPrinter = await PrinterFactory.shared.initPrinter("core-defs")
-        coreDefPrinter.preamble()
-        generateUnsafePointerHelpers(coreDefPrinter)
-        generateEnums(coreDefPrinter, cdef: nil, values: jsonApi.globalEnums, prefix: "")
-        await generateBuiltinClasses(values: jsonApi.builtinClasses, outputDir: generatedBuiltinDir)
-        await generateUtility(values: jsonApi.utilityFunctions, outputDir: generatedBuiltinDir)
-        await generateClasses (values: jsonApi.classes, outputDir: generatedDir)
+  func run() async throws {
+    let coreDefPrinter = await PrinterFactory.shared.initPrinter("core-defs")
+    coreDefPrinter.preamble()
+    generateUnsafePointerHelpers(coreDefPrinter)
+    generateEnums(coreDefPrinter, cdef: nil, values: jsonApi.globalEnums, prefix: "")
+    await generateBuiltinClasses(values: jsonApi.builtinClasses, outputDir: generatedBuiltinDir)
+    await generateUtility(values: jsonApi.utilityFunctions, outputDir: generatedBuiltinDir)
+    await generateClasses(values: jsonApi.classes, outputDir: generatedDir)
 
-        generateCtorPointers (coreDefPrinter)
-        generateNativeStructures(coreDefPrinter, values: jsonApi.nativeStructures)
+    generateCtorPointers(coreDefPrinter)
+    generateNativeStructures(coreDefPrinter, values: jsonApi.nativeStructures)
 
-        if let generatedBuiltinDir {
-            coreDefPrinter.save (generatedBuiltinDir + "/core-defs.swift")
-        }
-
-        if singleFile {
-            await PrinterFactory.shared.save(outputDir + "/generated.swift")
-        }
+    if let generatedBuiltinDir {
+      coreDefPrinter.save(generatedBuiltinDir + "/core-defs.swift")
     }
+
+    if singleFile {
+      await PrinterFactory.shared.saveMultiplexed(outputDir + "/generated")
+      //   await PrinterFactory.shared.save(outputDir + "/generated.swift")
+    }
+  }
 }
 
 let semaphore = DispatchSemaphore(value: 0)
 let _ = Task {
-    let generator = Generator()
-    try! await generator.run()
-    semaphore.signal()
+  let generator = Generator()
+  try! await generator.run()
+  semaphore.signal()
 }
 semaphore.wait()
 
