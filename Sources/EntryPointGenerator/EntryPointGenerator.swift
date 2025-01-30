981fc47c9da99ba61412ad5aff43e12b53b5fabf
Sam Deane <sam@elegantchaos.com>
2025-01-30 10:55:41 +0000

    Add a verbose option, off by default.

--- Sources/EntryPointGenerator/EntryPointGenerator.swift
+++ Sources/EntryPointGenerator/EntryPointGenerator.swift
@@ -18,12 +18,15 @@ struct EntryPointGenerator: ParsableCommand {
     @Argument(help: "Source files containing @Godot classes to generate entry points for.")
     var sourceFiles: [String]
 
+    @Option(name: .shortAndLong, help: "Produce more output.") 
+    var verbose: Bool = false
+
     mutating func run() throws {
-        let visitor = GodotMacroSearchingVisitor(viewMode: .all)
+        let visitor = GodotMacroSearchingVisitor(viewMode: .all, logger: verbose ? logVerbose : nil)
         
-        print("Scanning source files...")
+        logVerbose("Scanning source files...")
         for file in sourceFiles {
-            print("Scanning '\(file)'...")
+            logVerbose("Scanning '\(file)'...")
             let source = try String(contentsOf: URL(fileURLWithPath: file))
             let fileSyntax = Parser.parse(source: source)
             
@@ -31,9 +34,7 @@ struct EntryPointGenerator: ParsableCommand {
         }
         
         let names = visitor.classes
-            .map { name in
-                "    \(name).self"
-            }
+            .map { name in "    \(name).self" }
             .joined(separator: ",\n")
         
         let source = """
@@ -45,9 +46,22 @@ struct EntryPointGenerator: ParsableCommand {
         
         """
         
-        print("Writing \(visitor.classes.count) to '\(outputFile)'...")
+        let count = visitor.classes.count
+        logVerbose("Writing \(count) to '\(outputFile)'...")
         let outputURL = URL(fileURLWithPath: outputFile)
         try source.write(to: outputURL, atomically: true, encoding: .utf8)
-        print("Success! Entry point is `swift_entry_point`.")
+        log("Generated swift_entry_point, registering \(count) classes, in \(outputURL.lastPathComponent).")
+    }
+
+    /// Log a message to the console.
+    func log(_ message: String) {
+        print(message)
+    }
+
+    /// Log a message to the console if verbose mode is enabled.
+    func logVerbose(_ message: String) {
+        if verbose {
+            print(message)
+        }
     }
 }
