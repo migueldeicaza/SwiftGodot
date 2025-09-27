dc0ca550c5e7645202dfc52ce2cf61b944c1227d
Sravan Kumar <sravankumarkaruturi@gmail.com>
2025-09-27 06:41:22 -0700

    Fixed a typo and changed the mac OS from 13 to 14. (#732)
    Co-authored-by: Sravan <sravan.karuturi@strivr.com>

--- Sources/SwiftGodot/SwiftGodot.docc/Resources/Tutorials/Sources/Package-starter-3.swift
+++ Sources/SwiftGodot/SwiftGodot.docc/Resources/Tutorials/Sources/Package-starter-3.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SimpleRunnerDriver",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
