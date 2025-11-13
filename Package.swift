2f8688d9822aa6f09f511dd6fb7941b06c30e10c
Miguel de Icaza <miguel@gnome.org>
2025-11-13 17:43:24 -0500

    Actually, we need iOS 17

--- Package.swift
+++ Package.swift
@@ -288,7 +288,7 @@ let package = Package(
     name: "SwiftGodot",
     platforms: [
         .macOS(.v14),
-        .iOS (.v15)
+        .iOS (.v17)
     ],
     products: products,
     dependencies: [
